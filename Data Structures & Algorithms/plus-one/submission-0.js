3a2e52cee898a29ab22d52802e0dc8cb0f250c4c
Akshay Singh <akshay2898.as@gmail.com>
2026-07-13 16:47:27 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    plusOne(digits) {
+        for(let i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        digits.unshift(1);
+        return digits;
+    }
+}
