c3d3a90fe2ef23c52eb3fe3ea2c4436a53935f9b
Akshay Singh <akshay2898.as@gmail.com>
2026-07-24 12:37:36 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+      let maxFreq=0;
+      let left=0;
+      let maxLength=0;
+      const freq=new Map();
+      for(let right=0;right<s.length;right++){
+        const char=s[right];
+        freq.set(char,(freq.get(char)||0)+1);
+        maxFreq=Math.max(maxFreq,freq.get(char));
+
+        while((right-left+1)-maxFreq>k){
+            const leftChar=s[left];
+            freq.set(leftChar,freq.get(leftChar)-1);
+            left++;
+        }
+         maxLength=Math.max(maxLength,right-left+1);
+      
+      }
+     return maxLength;
+    }
+}
