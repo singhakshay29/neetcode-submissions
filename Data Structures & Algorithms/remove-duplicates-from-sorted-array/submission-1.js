dd4b95f13717a9166791fafc1fb16e5f0a438e96
Akshay Singh <akshay2898.as@gmail.com>
2026-08-07 13:15:15 +0530

    Add: remove-duplicates-from-sorted-array - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums) {
+        let left=0;
+        for(let right=1;right<nums.length;right++){
+            if(nums[left]!==nums[right]){
+                left++
+                nums[left]=nums[right];
+            }
+        }
+        return left+1;
+    }
+}
